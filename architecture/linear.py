53805ee25b221516b88250b3912b8b18f2ed4f1e
VicBrienen <2x5chmddbj@privaterelay.appleid.com>
2026-02-27 21:14:51 +0100

    implement Linear layer with Kaiming initialization

---
+++ architecture/linear.py
@@ -0,0 +1,24 @@
+import torch
+from torch import nn
+
+
+class Linear(nn.Module):
+    def __init__(self, in_features, out_features, bias=False):
+        super().__init__()
+
+         # Kaiming initialization https://arxiv.org/pdf/1502.01852
+        self.weights = nn.Parameter(torch.randn((in_features, out_features)) * (2 / in_features) ** 0.5)
+
+        # optional bias
+        if bias:
+            self.bias = nn.Parameter(torch.zeros(out_features))
+        else:
+            self.bias = None
+    
+    def forward(self, x): # (batch, in_features)
+        x = x @ self.weights
+
+        if self.bias is not None:
+            x = x + self.bias
+        
+        return x # (batch, in_features)

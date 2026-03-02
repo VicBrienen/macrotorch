e2f7a1185dee0995e1de961c1c44c399aa075593
VicBrienen <2x5chmddbj@privaterelay.appleid.com>
2026-03-02 20:45:23 +0100

    remove optional bias

--- _1958_Perceptron/linear.py
+++ _1958_Perceptron/linear.py
@@ -8,21 +8,14 @@ from torch import nn
 
 
 class Linear(nn.Module):
-    def __init__(self, in_features, out_features, bias=False):
+    def __init__(self, in_features, out_features, bias=True):
         super().__init__()
 
         self.weights = nn.Parameter(torch.randn(in_features, out_features) * 0.01)
-
-        # optional bias
-        if bias:
-            self.bias = nn.Parameter(torch.zeros(out_features))
-        else:
-            self.bias = None
+        self.bias = nn.Parameter(torch.zeros(out_features))
     
     def forward(self, x): # (batch, in_features)
-        x = x @ self.weights
-
-        if self.bias is not None:
-            x = x + self.bias
+        
+        x = x @ self.weights + self.bias
         
         return x # (batch, out_features)

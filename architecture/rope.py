218b55033910b403df115e0411b1212d5228d772
VicBrienen <2x5chmddbj@privaterelay.appleid.com>
2026-03-02 14:34:56 +0100

    add file for rope

---
+++ architecture/rope.py
@@ -0,0 +1,13 @@
+"""
+RoFormer: Enhanced Transformer with Rotary Position Embedding
+https://arxiv.org/pdf/2104.09864
+"""
+
+import torch
+from torch import nn
+
+class RoPE(nn.Module):
+    def __init__(self, dim, ):
+        super().__init__()
+
+        pass

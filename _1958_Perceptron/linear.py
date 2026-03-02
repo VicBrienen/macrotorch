b0c5e2c448fb3d058600bcd65ee0af83993d6eef
VicBrienen <2x5chmddbj@privaterelay.appleid.com>
2026-03-02 21:08:05 +0100

    fix linear

--- _1958_Perceptron/linear.py
+++ _1958_Perceptron/linear.py
@@ -8,14 +8,14 @@ from torch import nn
 
 
 class Linear(nn.Module):
-    def __init__(self, in_features, out_features, bias=True):
+    def __init__(self, in_features, out_features):
         super().__init__()
 
         self.weights = nn.Parameter(torch.randn(in_features, out_features) * 0.01)
         self.bias = nn.Parameter(torch.zeros(out_features))
     
     def forward(self, x): # (batch, in_features)
-        
+
         x = x @ self.weights + self.bias
         
         return x # (batch, out_features)

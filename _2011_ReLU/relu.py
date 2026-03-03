ae48a448d00de880f67e625243e944fde5002ee5
VicBrienen <2x5chmddbj@privaterelay.appleid.com>
2026-03-03 12:32:19 +0100

    fix citation

--- _2011_ReLU/relu.py
+++ _2011_ReLU/relu.py
@@ -1,8 +1,9 @@
+"""
+Sparse Rectifier Neural Networks
+https://proceedings.mlr.press/v15/glorot11a/glorot11a.pdfDeep
+"""
+
 import torch
 
 def relu(x):
-    """
-    Sparse Rectifier Neural Networks
-    https://proceedings.mlr.press/v15/glorot11a/glorot11a.pdfDeep
-    """
     return torch.clamp(x, 0)

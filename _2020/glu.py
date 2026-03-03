ab35a35f5899675fcc716362fba68b9c802690d3
VicBrienen <2x5chmddbj@privaterelay.appleid.com>
2026-03-04 00:28:50 +0100

    simplify GLU

--- _2020/glu.py
+++ _2020/glu.py
@@ -8,6 +8,7 @@ from torch import nn
 from _2015.kaiming import Linear
 from _2016.glu import glu
 
+
 class GLU(nn.Module):
     def __init__(self, dim, bias=False, activation=None, mlp_ratio=4, use_glu=True):
         super().__init__()
@@ -15,13 +16,9 @@ class GLU(nn.Module):
         self.activation = activation
         self.use_glu = use_glu
 
-        if self.use_glu:
-            mlp_ratio = 4 * 2 / 3 # ensure same parameter count as regular MLP
-            hidden_dim = int(dim * mlp_ratio)
-            self.linear_gate = Linear(dim, hidden_dim, bias) # additional upward gating projection for glu
-
-        else:
-            hidden_dim = dim * mlp_ratio
+        mlp_ratio = 4 * 2 / 3 # ensure same parameter count as regular MLP
+        hidden_dim = int(dim * mlp_ratio)
+        self.linear_gate = Linear(dim, hidden_dim, bias) # additional upward gating projection for glu
 
         # regular MLP up and down projection
         self.linear1 = Linear(dim, hidden_dim, bias)
@@ -29,14 +26,9 @@ class GLU(nn.Module):
 
     def forward(self, x):
 
-        if self.use_glu:
-            gate_value = self.linear_gate(x)
-            x = self.linear1(x)
-            x = glu(x, gate_value, self.activation)
-
-        else:
-            x = self.linear1(x)
-            x = self.activation(x)
+        gate_value = self.linear_gate(x)
+        x = self.linear1(x)
+        x = glu(x, gate_value, self.activation)
 
         x = self.linear2(x) # standard down projection back to input dimension
 

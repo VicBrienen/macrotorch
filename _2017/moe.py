d6bd0d814bcdf093f2633de559ae2f62802a79b5
VicBrienen <2x5chmddbj@privaterelay.appleid.com>
2026-03-04 23:12:18 +0100

    fix LLaMA transformer block

--- _2017/moe.py
+++
@@ -1,5 +0,0 @@
-"""
-Outrageously Large Neural Networks: The Sparsely-Gated Mixture-of-Experts Layer
-https://arxiv.org/pdf/1701.06538
-"""
-
--- _2023/LLaMA.py
+++ _2023/LLaMA.py
@@ -5,18 +5,19 @@ https://arxiv.org/pdf/2302.13971
 
 import torch
 from torch import nn
-from _2016.glu import silu
 from _2017.transformer import MultiheadAttention
+from _2017.silu import silu
 from _2019.rmsnorm import RMSNorm
-from _2020.glu import MLP
+from _2020.glu import GLU
 
+# very similar to most open source LLMs
 
-class DecoderBlock(nn.Module):
+class LLaMABlock(nn.Module):
     def __init__(self, embed_dim, heads, mlp_ratio):
         super().__init__()
 
         self.attention = MultiheadAttention(embed_dim, heads, causal=True) # causal self attention
-        self.mlp = MLP(embed_dim, bias=False, activation=silu, mlp_ratio=mlp_ratio, use_glu=True) # swish gated linear unit
+        self.mlp = GLU(embed_dim, bias=False, activation=silu, mlp_ratio=mlp_ratio, use_glu=True) # swish gated linear unit
 
         self.norm1 = RMSNorm(embed_dim)
         self.norm2 = RMSNorm(embed_dim)
